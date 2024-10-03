285adf838a552aaebc3e8568561d26366466966c
BasavarajHadli <basavaraj.hadli@gmail.com>
2024-10-03 16:39:27 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -15,7 +15,7 @@ terraform {
 }
 provider "azurerm" {
   features {}
-  skip_provider_registration = "true"
+  #skip_provider_registration = "true"
   # Connection to Azure
   subscription_id = var.subscription_id
   client_id = var.client_id
